304daa491b2b0f40fb5dc7865ad3052e1d416e6d
igarmatenko <cheef.che@gmail.com>
2013-10-01 19:54:26 +0400

    Revomed some ckeditor plugins

--- app/assets/javascripts/admin/spree_rich_cms.js
+++ app/assets/javascripts/admin/spree_rich_cms.js
@@ -1,4 +1,3 @@
 //= require admin/spree_backend
-//= require ckeditor/ckeditor.js
-//= require ckeditor/adapters/jquery.js
+//= require ckeditor.js
 //= require_tree .
---
+++ vendor/assets/javascripts/ckeditor.js
@@ -0,0 +1,5 @@
+//= require ckeditor/ckeditor.js
+//= require ckeditor/adapters/jquery.js
+//= require ckeditor/config.js
+//= require_tree ./ckeditor/lang
+//= require_tree ./ckeditor/plugins
