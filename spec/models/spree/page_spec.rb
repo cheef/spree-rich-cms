a29a0ed1465f099cb3d542776dca11a5eb38152c
igarmatenko <cheef.che@gmail.com>
2013-11-24 15:53:39 +0400

    Add test for decorating functionality

--- spec/models/spree/page_spec.rb
+++ spec/models/spree/page_spec.rb
@@ -3,5 +3,9 @@ require 'spec_helper'
 describe Spree::Page do
   let(:page) { build :page }
   subject { page }
+
   it { should be_valid }
+  it "should be decorated by dummy application" do
+    should respond_to :my_method
+  end
 end
